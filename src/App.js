d154f43f2ec0dd57abf3155cf70b37439b139276
JMurphy1 <murphy.johne@gmail.com>
2022-12-20 13:55:08 -0500

    spread and rest operators and updated README.md

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import * as React from 'react';
 
-
-
-// function getTitle(title){
-//   return title;
-// }
-
-// const welcome = {
-//   greeting: 'Hey',
-//   title: 'React',
-// }
-
 const App = () => {
   const stories = [
     {
       title: 'React',
-      url: 'https://reactjs.org',
+      url: 'https://reactjs.org/',
       author: 'Jordan Walke',
       num_comments: 3,
       points: 4,
@@ -23,18 +12,17 @@ const App = () => {
     },
     {
       title: 'Redux',
-      url: 'https://redux.js.org',
+      url: 'https://redux.js.org/',
       author: 'Dan Abramov, Andrew Clark',
       num_comments: 2,
       points: 5,
       objectID: 1,
-  
-  },
-  ];  
-  
+    },
+  ];
+
   const [searchTerm, setSearchTerm] = React.useState('React');
-  
-  const handleSearch = (event) =>{
+
+  const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
 
@@ -42,46 +30,48 @@ const App = () => {
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-return(
-  <div>
-    <h1>My Hacker Stories</h1>
-    <Search search={searchTerm} onSearch = {handleSearch}/>
-    <hr/>
-    <List list={searchedStories}/>
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
 
-  </div>
-);
+      <Search search={searchTerm} onSearch={handleSearch} />
+
+      <hr />
+
+      <List list={searchedStories} />
+    </div>
+  );
 };
 
-const Search = (props) => (
+const Search = ({ search, onSearch }) => (
   <div>
-    <label htmlFor = "search">Search: </label>
-    <input 
-    id = "search" 
-    type="text" 
-    value={props.search}
-    onChange={props.onSearch}
+    <label htmlFor="search">Search: </label>
+    <input
+      id="search"
+      type="text"
+      value={search}
+      onChange={onSearch}
     />
   </div>
 );
 
-const List = (props) => (
+const List = ({ list }) => (
   <ul>
-    {props.list.map((item) =>(
-      <Item key = {item.objectID} item={item}/>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
     ))}
-
   </ul>
 );
 
-const Item = (props) => (
+const Item = ({ item }) => (
   <li>
     <span>
-      <a href={props.item.url}>{props.item.title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </li>
 );
- export default App;
+
+export default App;
