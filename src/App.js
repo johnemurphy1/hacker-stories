8c7a83a9bd83171bf122d237a3bc38f0040fc5d6
JMurphy1 <murphy.johne@gmail.com>
2022-12-31 13:02:34 -0500

    React advanced state

--- src/App.js
+++ src/App.js
@@ -38,12 +38,29 @@ React.useEffect(() => {
 return [value, setValue];
 };
 
+const storiesReducer = (state,action) =>{
+  switch (action.type) {
+    case 'SET_STORIES':
+      return action.payload;
+    case 'REMOVE_STORY':
+      return state.filter(
+        (story) => action.payload.objectID !== story.objectID
+      );
+      default:
+        throw new Error();
+
+  }
+};
+
 const App = () => {
 
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search','React');  
 
-  const [stories, setStories] = React.useState([]);
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    []
+  );
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
 
@@ -51,8 +68,11 @@ const App = () => {
     setIsLoading(true);
     getAsyncStories()
       .then((result) => {
-      setStories(result.data.stories);
-      setIsLoading(false);
+        dispatchStories({
+          type: 'SET_STORIES',
+          payload: result.data.stories,
+        });
+        setIsLoading(false);
     })
       .catch(() => setIsError(true));
   }, []);
@@ -61,7 +81,10 @@ const App = () => {
     const newStories = stories.filter(
       (story) => item.objectID !== story.objectID
     );
-    setStories(newStories);
+    dispatchStories({
+      type: 'SET_STORIES',
+      payload: newStories,
+    });
   };
   
   const handleSearch = (event) => {
