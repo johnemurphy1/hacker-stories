3fae7c34311b22ca278e613179f3a7bcc5c36aad
JMurphy1 <murphy.johne@gmail.com>
2022-12-31 20:29:30 -0500

    added data fetching

--- src/App.js
+++ src/App.js
@@ -74,6 +74,8 @@ const storiesReducer = (state, action) => {
   }
 };
 
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState(
@@ -91,11 +93,11 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({type: 'STORIES_FETCH_INIT'});
     
-    getAsyncStories()
-      .then((result) => {
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
+          payload: result.hits,
         });
     })
       .catch(() => 
@@ -151,7 +153,7 @@ const App = () => {
       )}
     </div>
   );
-};
+        };
  
 const InputWithLabel = ({
   id,  
